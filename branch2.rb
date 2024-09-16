5e124e70cce0fd2e78661bea7483df6f2f0e0431
Gavin-Shi <shi.1742@osu.edu>
2024-09-16 16:25:29 -0400

    Completing number 4 in catogory 1 and category 2 blue methods

--- branch2.rb
+++ branch2.rb
@@ -1,39 +1,66 @@
-#Amir Shaltami
-def calculate_mean(set)
-  total = set.sum
-  elements = set.length
-  mean = total / elements
-  puts "The mean of the set is #{mean}"
+# Amir Shaltami
+# Category 1 Convert a decimal number to binary, octal, or hexadecimal.
+def convert_to_binary(n)
+  puts "The decimal number #{n} converted to binary is #{n.to_s(2)}."
+end
+
+def convert_to_octal(n)
+  puts "The decimal number #{n} converted to octal is #{n.to_s(8)}."
+end
+
+def convert_to_hexadecimal(n)
+  puts "The decimal number #{n} converted to hexadecimal is #{n.to_s(16)}."
+end
+
+# Category 2 Blue methods
+def mean(data)
+  sum = data.sum
+  elements = data.length
+  mean = sum / elements
+  puts "The mean of the set is #{mean}."
+end
+
+def maximum(data)
+  max = data[0]
+  counter = 1
+  while counter < data.length
+    if data[counter] > max
+      max = data[counter]
+    end
+    counter += 1
+  end
+  puts "The largest value in the set is #{max}."
+end
+
+def fibonacci_numbers(limit, file_name)
+  sequence = []
+  a = 0
+  b = 1
+  while a <= limit
+    # Using the shovel operator to append to the array
+    sequence << a
+    previous_a = a
+    a = b
+    b = previous_a + b
+  end
+  File.open(file_name, 'w') do |file|
+    sequence.each { |num| file.puts num }
+  end
+  puts "Fibonacci numbers up to #{limit} have been saved to #{file_name}."
 end
 
 def farenheit_to_celsius(farenheit)
   celsius = (farenheit - 32.0) * 5.0/9.0
-  puts "The value #{farenheit} degrees celsius in farenheit is #{celsius} degrees farenheit"
+  puts "The value #{farenheit} degrees farenheit in celsius is #{celsius.round(2)} degrees celsius"
 end
 
-  def find_largest_value 
-    put "Input a set of values"
-    input = gets.chomp
-    set = set.new{input}
-    numbers = input.split.map(&:to_i)
-    largest = set.max
-    puts "The largest value in the set is #{largest}"
-      
-    end
-def fibonachi_numbers(limit)
-  put "Put in the first value"
-input = gets.chomp 
-first = input.to_f
-put "Put in the second value"
-userIn = gets.chomp
-second = userIn.to_f
-File.open("fibonachi_numbers.txt, w") do |file|
-  file.puts first  
-      first, second = second, first + second  
-end
-end
-end
-def main
-  
- calculate_mean ([13, 27, 45, 34, 506])
-farenheit_to_celsius(86.0)
+convert_to_binary(50)
+convert_to_octal(100)
+convert_to_hexadecimal(200)
+
+set = [13, 27, 45, 34, 506]
+
+mean(set)
+maximum(set)
+fibonacci_numbers(1000, "fibonacci_numbers.txt")
+farenheit_to_celsius(87.0)
