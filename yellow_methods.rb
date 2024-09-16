207cb37d5cc26c8ce26d190b254f3abaff83a7b2
Gavin-Shi <shi.1742@osu.edu>
2024-09-16 15:24:16 -0400

    Number 1 in Category 1

---
+++ yellow_methods.rb
@@ -0,0 +1,11 @@
+def squareRoot(n)
+  puts "The square root of #{n} is: #{Math.sqrt(n)}"
+end
+
+def cubeRoot(n)
+  puts "The cube root of #{n} is: #{Math.cbrt(n)}"
+end
+
+squareRoot(16)
+
+cubeRoot(27)
