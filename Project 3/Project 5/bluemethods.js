b6dfbcf90e6e36abb50a623415dd942386ba877d
Amir <amirshaltami2@gmail.com>
2024-11-06 18:28:37 -0500

    Amir's changes

---
+++ Project 3/Project 5/bluemethods.js
@@ -0,0 +1,10 @@
+function convertToBinary(n){
+console.log("The decimal number" + n "is" + n.toString(2) + "");
+}
+function convertToOctal(n){
+    console.log("The decimal number" + n "is" + n.toString(8) + "");
+    }
+    function convertToBinary(n){
+        console.log("The decimal number" + n "is" + n.toString(16) + "");
+        }
+
