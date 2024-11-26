364506973ea3144565cf3d5c44ecdf8a376b25f5
Gavin-Shi <shi.1742@osu.edu>
2024-11-25 23:58:35 -0500

    Redoing Project 6

--- Project 6/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true, 
-  length:{maximum: 10}
-  validates :email, presence: true,
-  length:{maximum: 50},
-      uniqueness: true, format: { with: /\A\w+\.\d+@osu\.edu\Z/i}
-  validates :password, presence: true, length: {minimum: 6}
-  has_secure_password
-end
