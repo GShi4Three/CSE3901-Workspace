ced9b61f874a97d589fe6c37174a029e8b3a7523
Khaled <117534908+djay04@users.noreply.github.com>
2024-12-02 16:20:41 -0500

    Altering passwordDigest and moving User.rb file from controller to model

--- Project 6/db/schema.rb
+++ Project 6/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_02_010615) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_02_210735) do
   create_table "courses", force: :cascade do |t|
     t.string "name", null: false
     t.string "semester", null: false
@@ -68,10 +68,10 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_02_010615) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password", null: false
     t.string "role", default: "student", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
