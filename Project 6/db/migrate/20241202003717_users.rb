5bc92a0fbf11e5fae4e48837ec90037bfc57d414
Khaled <117534908+djay04@users.noreply.github.com>
2024-12-01 19:40:10 -0500

    Migrated users

---
+++ Project 6/db/migrate/20241202003717_users.rb
@@ -0,0 +1,11 @@
+class Users < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|:
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password, null: false
+      t.string :role, null: false, default: "student"
+      t.timestamps
+    end
+  end
+end
