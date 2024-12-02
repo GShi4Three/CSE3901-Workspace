55b89945be4240cdfadb291739aa9a59f0bd97c4
Khaled <117534908+djay04@users.noreply.github.com>
2024-12-01 19:54:46 -0500

    Updated user and evaluations tables

--- Project 6/db/migrate/20241202003156_evaluations.rb
+++ Project 6/db/migrate/20241202003156_evaluations.rb
@@ -8,8 +8,8 @@ class Evaluations < ActiveRecord::Migration[8.0]
       t.integer :organization_score, null: false
       t.integer :pacing_score, null: false
       t.integer :professionalism_score, null: false
-      t.integer: total_score, null: false
-
+      
+      t.integer :total_score, null: false
       t.text :comment
       t.timestamps
     end
--- Project 6/db/migrate/20241202003717_users.rb
+++ Project 6/db/migrate/20241202003717_users.rb
@@ -2,7 +2,7 @@ class Users < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|:
       t.string :name, null: false
-      t.string :email, null: false
+      t.string :email, null: false, unique: true
       t.string :password, null: false
       t.string :role, null: false, default: "student"
       t.timestamps
