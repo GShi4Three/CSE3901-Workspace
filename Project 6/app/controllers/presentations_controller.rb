48e80f4bf0dfa881f6a5231eb2c3e16f9d1b131e
Gavin-Shi <shi.1742@osu.edu>
2024-12-03 04:51:07 -0500

    Implemented the ability to create and delete presentations

--- Project 6/app/controllers/presentations_controller.rb
+++ Project 6/app/controllers/presentations_controller.rb
@@ -8,15 +8,7 @@ class PresentationsController < ApplicationController
 
   def index_ta
     @presentations = Presentation.includes(:users).all
-  end
-
-  def authenticate_user
-    redirect_to '/', alert: 'Please log in first!' unless session[:user_id]
-  end
-
-  def authorize_ta
-    redirect_to '/presentations', alert: 'Access denied!' unless session[:user_id]
-  end
+  end 
   
   def new
     @presentation = Presentation.new
@@ -36,10 +28,27 @@ class PresentationsController < ApplicationController
     end
   end
 
+  def destroy
+    @presentation = Presentation.find(params[:id])
+    if @presentation.destroy
+      redirect_to presentations_ta_path, notice: 'Presentation deleted successfully.'
+    else
+      redirect_to presentations_ta_path, alert: 'Failed to delete presentation.'
+    end
+  end
+
   private
 
   def presentation_params
-    params.require(:presentation).permit(:title, user_ids: [])
+    params.require(:presentation).permit(:name, user_ids: [])
+  end
+
+  def authenticate_user
+    redirect_to '/', alert: 'Please log in first!' unless session[:user_id]
+  end
+
+  def authorize_ta
+    redirect_to '/presentations', alert: 'Access denied!' unless session[:user_id]
   end
 
 end
