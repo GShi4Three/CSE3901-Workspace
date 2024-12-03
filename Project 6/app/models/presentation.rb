08fc76b2f14d8b96b869bc17e2377283d589be4e
Gavin-Shi <shi.1742@osu.edu>
2024-12-02 19:18:01 -0500

    Updating models and seed file

--- Project 6/app/models/presentation.rb
+++ Project 6/app/models/presentation.rb
@@ -1,2 +1,5 @@
 class Presentation < ApplicationRecord
+  # Associations
+  has_many :evaluations
+  has_and_belongs_to_many :users, join_table: :presentation_creators
 end
--- Project 6/app/models/user.rb
+++ Project 6/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  # Associations
+  has_many :evaluations
+  has_and_belongs_to_many :presentations, join_table: :presentation_creators
 end
