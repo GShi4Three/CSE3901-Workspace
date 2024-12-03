45e518e2fd2452231232dce2e1aea46bb1960721
Khaled <117534908+djay04@users.noreply.github.com>
2024-12-02 20:35:05 -0500

    Finishing up integration

--- Project 6/db/migrate/20241202004035_courses.rb
+++ Project 6/db/migrate/20241202004035_courses.rb
@@ -1,7 +1,6 @@
 class Courses < ActiveRecord::Migration[8.0]
   def change
     create_table :courses do |t|
-
       t.string :name, null: false
       t.string :semester, null: false
       t.references :instructor, null: false, foreign_key: { to_table: :users }
